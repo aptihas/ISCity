c8199d1530c1eddcc3c0f93aed06aa911c086c25
macbookpro <macbookpro@VIVUSPC>
2017-12-24 16:31:34 +0300

    Админка достаточно готова, модуль пользователя готов, модуль ресурсоснабжающей компании готов. Надо сделать модуль управляющей компании и доделать стили.

--- ISCity/Controllers/AdminController.cs
+++ ISCity/Controllers/AdminController.cs
@@ -19,9 +19,51 @@ namespace ISCity.Controllers
             return View();
         }
 
+        public ActionResult ManageCompanyList()
+        {
+            var mcList = dbEnt.ManageCompany.ToList();
+            return View(mcList);
+        }
+
+
         public ActionResult AddManageCompany()
         {
             return View();
+        }
+
+        public ActionResult EditManageCompany(int id)
+        {
+            var mc = dbEnt.ManageCompany.Where(m => m.id == id).Select(m => m).FirstOrDefault();
+            var usr = dbEnt.Users.Where(u => u.manageCompany_id == id).Select(u => u).FirstOrDefault();
+
+            ManageCompanyModel mcm = new ManageCompanyModel { Category=mc.Category, Email=usr.Email, FirstName=usr.FirstName, HouseNumber=mc.HouseNumber, Name=mc.Name, SecondName=usr.SecondName, Street=mc.Street, ThirdName=usr.ThirdName};
+
+            ViewBag.UserID = usr.id;
+            ViewBag.MCID = mc.id;
+
+            return View(mcm);
+        }
+
+        [HttpPost]
+        public ActionResult EditManageCompany(ManageCompanyModel mc, int user_id, int mc_id)
+        {
+            var _usr = dbEnt.Users.Where(u => u.id == user_id).Select(u => u).FirstOrDefault();
+            var _mc = dbEnt.ManageCompany.Where(m => m.id == mc_id).Select(m => m).FirstOrDefault();
+
+            _mc.Name = mc.Name;
+            _mc.Street = mc.Street;
+            _mc.Category = mc.Category;
+            _mc.HouseNumber = mc.HouseNumber;
+
+            _usr.Email = mc.Email;
+            _usr.FirstName = mc.FirstName;
+            _usr.SecondName = mc.SecondName;
+            _usr.ThirdName = mc.ThirdName;
+
+            dbEnt.SaveChanges();
+
+            var mcList = dbEnt.ManageCompany.ToList();
+            return RedirectToAction("ManageCompanyList", mcList);
         }
 
         [HttpPost]
@@ -37,7 +79,7 @@ namespace ISCity.Controllers
                 dbEnt.ManageCompany.Add(_mcObj);
                 dbEnt.SaveChanges();
 
-                Users _usrObj = new Users { FirstName = mc.FirstName, SecondName = mc.SecondName, ThirdName = mc.ThirdName, Email = mc.Email, EmailConfirm=false };
+                Users _usrObj = new Users { FirstName = mc.FirstName, SecondName = mc.SecondName, ThirdName = mc.ThirdName, Email = mc.Email, EmailConfirm=false,manageCompany_id= _mcObj.id };
                 dbEnt.Users.Add(_usrObj);
                 dbEnt.SaveChanges();
 
--- ISCity/Controllers/ManageCompanyController.cs
+++ ISCity/Controllers/ManageCompanyController.cs
@@ -7,13 +7,85 @@ using System.Web.Mvc;
 
 namespace ISCity.Controllers
 {
+    
     [MyRoleAtribute(Roles = "ManageCompany")]
     public class ManageCompanyController : Controller
     {
+        DBISCityEntities dbEnt = new DBISCityEntities();
         // GET: ManageCompany
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult RequestList()
+        {
+            var _acc = (from a in dbEnt.Accounts where a.Users.Email == User.Identity.Name select a.Users).FirstOrDefault();
+            var _requests = dbEnt.UserRequests.Where(r => r.mangeCompany_id == _acc.manageCompany_id).Select(r => r);
+
+            return View(_requests);
+        }
+
+        public ActionResult DetailsRequest(int id)
+        {
+            var _requests = dbEnt.UserRequests.Where(r => r.id == id).Select(r => r).FirstOrDefault();
+            return View(_requests);
+        }
+
+        public ActionResult ConsolidateSubCompany(int id)
+        {
+            var _requests = dbEnt.UserRequests.Where(r => r.id == id).Select(r => r).FirstOrDefault();
+            ViewBag.SubCompanyList = dbEnt.SubCompany.ToList();
+            return View(_requests);
+        }
+
+        [HttpPost]
+        public ActionResult ConsolidateSubCompany(int id_request, int subCompany_id)
+        {
+            var _requests = dbEnt.UserRequests.Where(r => r.id == id_request).Select(r => r).FirstOrDefault();
+            _requests.subCompany_id = subCompany_id;
+            dbEnt.SaveChanges();
+            return RedirectToAction("DetailsRequest", new { id = id_request });
+        }
+
+
+        public ActionResult AddSubCompany()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSubCompany(SubCompany sc)
+        {
+            var _acc = (from a in dbEnt.Accounts where a.Users.Email == User.Identity.Name select a.Users).FirstOrDefault();
+            dbEnt.SubCompany.Add(new SubCompany { mangeCompany_id= (int)_acc.manageCompany_id, Name=sc.Name, Street=sc.Street, HouseNumber=sc.HouseNumber });
+            dbEnt.SaveChanges();
+            return RedirectToAction("SubCompanyList");
+        }
+
+        public ActionResult SubCompanyList()
+        {
+            var scList = dbEnt.SubCompany.ToList();
+            return View(scList);
+        }
+
+        public ActionResult EditSubCompany(int id)
+        {
+            var sc = dbEnt.SubCompany.Where(s => s.id == id).Select(s => s).FirstOrDefault();
+
+            return View(sc);
+        }
+
+        [HttpPost]
+        public ActionResult EditSubCompany(SubCompany _sc)
+        {
+            var sc = dbEnt.SubCompany.Where(s => s.id == _sc.id).Select(s => s).FirstOrDefault();
+            sc.Name = _sc.Name;
+            sc.Street = _sc.Street;
+            sc.HouseNumber = _sc.HouseNumber;
+            dbEnt.SaveChanges();
+
+            return RedirectToAction("SubCompanyList");
         }
     }
 }
